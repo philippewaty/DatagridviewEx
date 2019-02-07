d720de2d1d1da0bc77ae43f6cd84106c86fba584
Philippe Waty <pwa8460008@hotmail.com>
2019-02-07 15:54:06 +0100

    Added FormColumnsConfig

---
+++ master/DataGridViewEx/FormColumnsConfig.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace DataGridViewEx
+{
+  partial class FormColumnsConfig
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      this.components = new System.ComponentModel.Container();
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormColumnsConfig));
+      this.btnRestore = new System.Windows.Forms.Button();
+      this.btnClose = new System.Windows.Forms.Button();
+      this.btnDown = new System.Windows.Forms.Button();
+      this.btnUp = new System.Windows.Forms.Button();
+      this.lstColumns = new System.Windows.Forms.CheckedListBox();
+      this.ToolTip1 = new System.Windows.Forms.ToolTip(this.components);
+      this.SuspendLayout();
+      // 
+      // btnRestore
+      // 
+      this.btnRestore.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.btnRestore.Image = global::DataGridViewEx.Properties.Resources.recur;
+      this.btnRestore.Location = new System.Drawing.Point(243, 78);
+      this.btnRestore.Name = "btnRestore";
+      this.btnRestore.Size = new System.Drawing.Size(27, 27);
+      this.btnRestore.TabIndex = 8;
+      this.ToolTip1.SetToolTip(this.btnRestore, "Restore order and visibility");
+      this.btnRestore.UseVisualStyleBackColor = true;
+      this.btnRestore.Click += new System.EventHandler(this.btnRestore_Click);
+      // 
+      // btnClose
+      // 
+      this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+      this.btnClose.DialogResult = System.Windows.Forms.DialogResult.Cancel;
+      this.btnClose.Image = global::DataGridViewEx.Properties.Resources.fileclose;
+      this.btnClose.Location = new System.Drawing.Point(243, 199);
+      this.btnClose.Name = "btnClose";
+      this.btnClose.Size = new System.Drawing.Size(27, 27);
+      this.btnClose.TabIndex = 9;
+      this.btnClose.UseVisualStyleBackColor = true;
+      // 
+      // btnDown
+      // 
+      this.btnDown.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.btnDown.Image = global::DataGridViewEx.Properties.Resources.down;
+      this.btnDown.Location = new System.Drawing.Point(243, 45);
+      this.btnDown.Name = "btnDown";
+      this.btnDown.Size = new System.Drawing.Size(27, 27);
+      this.btnDown.TabIndex = 7;
+      this.ToolTip1.SetToolTip(this.btnDown, "Move down");
+      this.btnDown.UseVisualStyleBackColor = true;
+      this.btnDown.Click += new System.EventHandler(this.btnDown_Click);
+      // 
+      // btnUp
+      // 
+      this.btnUp.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+      this.btnUp.Image = global::DataGridViewEx.Properties.Resources.up;
+      this.btnUp.Location = new System.Drawing.Point(243, 12);
+      this.btnUp.Name = "btnUp";
+      this.btnUp.Size = new System.Drawing.Size(27, 27);
+      this.btnUp.TabIndex = 6;
+      this.ToolTip1.SetToolTip(this.btnUp, "Move up");
+      this.btnUp.UseVisualStyleBackColor = true;
+      this.btnUp.Click += new System.EventHandler(this.btnUp_Click);
+      // 
+      // lstColumns
+      // 
+      this.lstColumns.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+      this.lstColumns.FormattingEnabled = true;
+      this.lstColumns.Location = new System.Drawing.Point(12, 12);
+      this.lstColumns.Name = "lstColumns";
+      this.lstColumns.Size = new System.Drawing.Size(225, 214);
+      this.lstColumns.TabIndex = 5;
+      this.lstColumns.ItemCheck += new System.Windows.Forms.ItemCheckEventHandler(this.lstColumns_ItemCheck);
+      // 
+      // FormColumnsConfig
+      // 
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.None;
+      this.CancelButton = this.btnClose;
+      this.ClientSize = new System.Drawing.Size(282, 232);
+      this.Controls.Add(this.btnRestore);
+      this.Controls.Add(this.btnClose);
+      this.Controls.Add(this.btnDown);
+      this.Controls.Add(this.btnUp);
+      this.Controls.Add(this.lstColumns);
+      this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+      this.Name = "FormColumnsConfig";
+      this.StartPosition = System.Windows.Forms.FormStartPosition.CenterParent;
+      this.Text = "Columns configuration";
+      this.Load += new System.EventHandler(this.FormColumnsConfig_Load);
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    internal System.Windows.Forms.Button btnRestore;
+    internal System.Windows.Forms.ToolTip ToolTip1;
+    internal System.Windows.Forms.Button btnClose;
+    internal System.Windows.Forms.Button btnDown;
+    internal System.Windows.Forms.Button btnUp;
+    internal System.Windows.Forms.CheckedListBox lstColumns;
+  }
+}
---
+++ master/DataGridViewEx/FormColumnsConfig.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DataGridViewEx
+{
+  public partial class FormColumnsConfig : Form
+  {
+
+    /// <summary>
+    /// Class used to store DataGridView column info
+    /// </summary>
+    private class ItemData
+    {
+
+      private string _columnName;
+
+      private string _Text;
+      public ItemData(string columnName, string text)
+      {
+        _columnName = columnName;
+        _Text = text;
+      }
+
+      public string ColumnName
+      {
+        get { return _columnName; }
+        set { _columnName = value; }
+      }
+
+      public string Text
+      {
+        get { return _Text; }
+        set { _Text = value; }
+      }
+
+      public override string ToString()
+      {
+        return _Text;
+      }
+
+    }
+
+    private DataGridViewEx _dataGridView;
+    private bool _loading;
+
+    public FormColumnsConfig(DataGridViewEx dataGridView)
+    {
+      InitializeComponent();
+      _dataGridView = dataGridView;
+    }
+
+    private void FormColumnsConfig_Load(object sender, EventArgs e)
+    {
+      _loading = true;
+      //*** Add dumies items
+      for (int i = 0; i <= _dataGridView.Columns.Count - 1; i++)
+      {
+        lstColumns.Items.Add("", false);
+      }
+      //*** Fill in the real items at the right place according to displayIndex propertie
+      for (int i = 0; i <= _dataGridView.Columns.Count - 1; i++)
+      {
+        ItemData item = new ItemData(_dataGridView.Columns[i].Name, _dataGridView.Columns[i].HeaderText);
+        lstColumns.Items[_dataGridView.Columns[i].DisplayIndex] = item;
+        lstColumns.SetItemChecked(i, _dataGridView.Columns[i].Visible);
+      }
+      if (lstColumns.Items.Count > 0)
+        lstColumns.SelectedIndex = 0;
+      _loading = false;
+
+    }
+
+    private void lstColumns_ItemCheck(object sender, ItemCheckEventArgs e)
+    {
+      if (_loading)
+        return;
+      if (e.Index > -1 & e.Index < lstColumns.Items.Count)
+      {
+        ItemData item = (ItemData)lstColumns.Items[e.Index];
+        _dataGridView.Columns[item.ColumnName].Visible = e.NewValue == CheckState.Checked;
+      }
+
+    }
+
+    private void btnUp_Click(object sender, EventArgs e)
+    {
+      if (lstColumns.SelectedIndex == 0)
+        return;
+
+      int index = lstColumns.SelectedIndex;
+      ItemData item = (ItemData)lstColumns.SelectedItem;
+      lstColumns.Items.Insert(index - 1, lstColumns.SelectedItem);
+      lstColumns.SetItemChecked(index - 1, lstColumns.GetItemChecked(index + 1));
+      lstColumns.Items.RemoveAt(index + 1);
+      lstColumns.SelectedIndex = index - 1;
+      _dataGridView.Columns[item.ColumnName].DisplayIndex = lstColumns.SelectedIndex;
+
+    }
+
+    private void btnDown_Click(object sender, EventArgs e)
+    {
+      if (lstColumns.SelectedIndex != -1 & lstColumns.SelectedIndex == lstColumns.Items.Count - 1)
+        return;
+
+      int index = lstColumns.SelectedIndex;
+      ItemData item = (ItemData)lstColumns.SelectedItem;
+      lstColumns.Items.Insert(index + 2, lstColumns.SelectedItem);
+      lstColumns.SetItemChecked(index + 2, lstColumns.GetItemChecked(index));
+      lstColumns.Items.RemoveAt(index);
+      lstColumns.SelectedIndex = index + 1;
+      _dataGridView.Columns[item.ColumnName].DisplayIndex = lstColumns.SelectedIndex;
+
+    }
+
+    private void btnRestore_Click(object sender, EventArgs e)
+    {
+      lstColumns.Items.Clear();
+      //*** Fill in the items
+      for (int i = 0; i <= _dataGridView.Columns.Count - 1; i++)
+      {
+        ItemData item = new ItemData(_dataGridView.Columns[i].Name, _dataGridView.Columns[i].HeaderText);
+        lstColumns.Items.Add(item);
+        lstColumns.SetItemChecked(i, true);
+        _dataGridView.Columns[item.ColumnName].DisplayIndex = _dataGridView.Columns[item.ColumnName].Index;
+        _dataGridView.Columns[item.ColumnName].Visible = true;
+      }
+      if (lstColumns.Items.Count > 0)
+        lstColumns.SelectedIndex = 0;
+
+    }
+  }
+}
