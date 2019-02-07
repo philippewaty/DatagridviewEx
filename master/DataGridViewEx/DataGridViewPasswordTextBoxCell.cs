c12e5dec4a63b24eead25516955fcce1dd996466
Philippe Waty <pwa8460008@hotmail.com>
2019-02-07 15:05:45 +0100

    Added PasswordTextBox column & cell

---
+++ master/DataGridViewEx/DataGridViewPasswordTextBoxCell.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace DataGridViewEx
+{
+  public class DataGridViewPasswordTextBoxCell : System.Windows.Forms.DataGridViewTextBoxCell
+  {
+
+
+    private char editingControlPasswordChar;
+
+    private bool editingControlUseSystemPasswordChar;
+    public char PasswordChar { get; set; } = '*';
+
+    public bool UseSystemPasswordChar { get; set; }
+
+    public override object Clone()
+    {
+      DataGridViewPasswordTextBoxCell copy = (DataGridViewPasswordTextBoxCell)base.Clone();
+
+      copy.PasswordChar = this.PasswordChar;
+      copy.UseSystemPasswordChar = this.UseSystemPasswordChar;
+
+      return copy;
+    }
+
+    protected override object GetFormattedValue(object value, int rowIndex, ref System.Windows.Forms.DataGridViewCellStyle cellStyle, System.ComponentModel.TypeConverter valueTypeConverter, System.ComponentModel.TypeConverter formattedValueTypeConverter, System.Windows.Forms.DataGridViewDataErrorContexts context)
+    {
+      object formattedValue;
+
+      if (this.UseSystemPasswordChar && value != null)
+      {
+        //Display the system password character in place of each actual character.
+        //TODO: Determine the actual system password character instead of hard-coding this value.
+        formattedValue = new string(Convert.ToChar(0x25cf), ((string)value).Length);
+      }
+      else if (this.PasswordChar != char.MinValue && value != null)
+      {
+        //Display the user-defined password character in place of each actual character.
+        formattedValue = new string(this.PasswordChar, ((string)value).Length);
+      }
+      else
+      {
+        //Display the value as is.
+        formattedValue = base.GetFormattedValue(value, rowIndex, ref cellStyle, valueTypeConverter, formattedValueTypeConverter, context);
+      }
+
+      return formattedValue;
+    }
+
+    public override void InitializeEditingControl(int rowIndex, object initialFormattedValue, System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle)
+    {
+      base.InitializeEditingControl(rowIndex, initialFormattedValue, dataGridViewCellStyle);
+
+      //Remember the current password properties of the editing control.
+
+      //Set the new password properties of the editing control.
+      // ERROR: Not supported in C#: WithStatement
+
+    }
+
+    public override void DetachEditingControl()
+    {
+      base.DetachEditingControl();
+
+      //Reset the old password properties of the editing control.
+      // ERROR: Not supported in C#: WithStatement
+
+    }
+
+  }
+
+}
---
+++ master/DataGridViewEx/DataGridViewPasswordTextBoxColumn.cs
@@ -0,0 +1,105 @@
+﻿using System.ComponentModel;
+using System.Windows.Forms;
+
+namespace DataGridViewEx
+{
+
+  public class DataGridViewPasswordTextBoxColumn : System.Windows.Forms.DataGridViewColumn
+  {
+
+    private char _passwordChar;
+
+    private bool _useSystemPasswordChar;
+    [Category("Password")]
+    public char PasswordChar
+    {
+      get { return this._passwordChar; }
+      set
+      {
+        if (this._passwordChar != value)
+        {
+          this._passwordChar = value;
+
+          DataGridViewPasswordTextBoxCell cell = this.CellTemplate as DataGridViewPasswordTextBoxCell;
+
+          if (cell != null)
+          {
+            //Update the template cell.
+            cell.PasswordChar = value;
+          }
+
+          if (this.DataGridView != null)
+          {
+            //Update each existing cell in the column.
+            foreach (DataGridViewRow row in this.DataGridView.Rows)
+            {
+              cell = row.Cells[this.Index] as DataGridViewPasswordTextBoxCell;
+
+              if (cell != null)
+              {
+                cell.PasswordChar = value;
+              }
+            }
+
+            //Force a repaint so the grid reflects the current property value.
+            this.DataGridView.Refresh();
+          }
+        }
+      }
+    }
+
+    [Category("Password")]
+    public bool UseSystemPasswordChar
+    {
+      get { return this._useSystemPasswordChar; }
+      set
+      {
+        if (this._useSystemPasswordChar != value)
+        {
+          this._useSystemPasswordChar = value;
+
+          DataGridViewPasswordTextBoxCell cell = this.CellTemplate as DataGridViewPasswordTextBoxCell;
+
+          if (cell != null)
+          {
+            //Update the template cell.
+            cell.UseSystemPasswordChar = value;
+          }
+
+          if (this.DataGridView != null)
+          {
+            //Update each existing cell in the column.
+            foreach (DataGridViewRow row in this.DataGridView.Rows)
+            {
+              cell = row.Cells[this.Index] as DataGridViewPasswordTextBoxCell;
+
+              if (cell != null)
+              {
+                cell.UseSystemPasswordChar = value;
+              }
+            }
+
+            //Force a repaint so the grid reflects the current property value.
+            this.DataGridView.Refresh();
+          }
+        }
+      }
+    }
+
+    public DataGridViewPasswordTextBoxColumn(): base(new DataGridViewPasswordTextBoxCell())
+    {
+    }
+
+    public override object Clone()
+    {
+      DataGridViewPasswordTextBoxColumn copy = (DataGridViewPasswordTextBoxColumn)base.Clone();
+
+      copy.PasswordChar = this._passwordChar;
+      copy.UseSystemPasswordChar = this._useSystemPasswordChar;
+
+      return copy;
+    }
+
+  }
+
+}
